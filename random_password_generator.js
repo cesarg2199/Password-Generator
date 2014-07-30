51f4ab4a3b7a1ce264a60dd6384ae869643489bf
cesarg2199 <cesarguerrero2199@yahoo.com>
2014-07-29 22:38:06 -0400

    Random password generator added

---
+++ random_password_generator.js
@@ -0,0 +1,55 @@
+//Random password generator v1.0
+//By Cesar Guerrero
+
+function shuffle() {
+    //Getting array of alphabet
+    var myArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+    //Selecting one letter from array randomly
+    var rand = myArray[Math.floor(Math.random() * myArray.length)];
+
+    //Returning the randomly selected letter
+    return rand;
+}
+
+function key() {
+    //Chosing the numbers one by one
+    var firstNumber = Math.floor((Math.random() * 9) + 1);
+    var secondNumber = Math.floor((Math.random() * 9) + 1);
+
+    //Chosing the letters one by one
+    var firstLetter = shuffle();
+    var secondLetter = shuffle();
+
+    //Concatinating indiviual values together as a set of 4
+    var pin = firstNumber + firstLetter + secondNumber + secondLetter;
+
+    //Returning the set of 4
+    return pin;
+}
+
+function generate() {
+    //Declaring sets
+    var text1 = key();
+    var text2 = key();
+    var text3 = key();
+    var text4 = key();
+
+    //Concatinating sets together to create a 16 combination(19 Charecters toral with dashes)
+    var final = text1 + "-" + text2 + "-" + text3 + "-" + text4;
+
+    //Returning long password
+    return final;
+}
+
+
+$("#button").click(function () {
+    //Setting the value of the password field with what was generated
+    var $pass = $("#p").val(generate);
+
+    //Setting was on the password field to the confirm password field
+    $("#cp").val($pass.val());
+
+    //How to show user what was generated
+    $("#getter").text($pass.val());
+});

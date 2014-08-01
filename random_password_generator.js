e3f3a4a8c1722027f0d2b7f220ad13033dc7734e
cesarg2199 <cesarguerrero2199@yahoo.com>
2014-07-31 21:36:17 -0400

    New encryption method

--- random_password_generator.js
+++ random_password_generator.js
@@ -1,14 +1,23 @@
-//Random password generator v1.1
+//Random password generator v2.0
 /*
-    Revisions:
-    Can now use them as classes instead of id's which meand you can have the confirm password field show up many times
+
+Revisions:
+
+Can now use them as classes instead of id's, which means you can have the confirm password field show up many times
     
-    Note if you try to use the confirm password field as password,             the two results will be different
+Passwords will include uppercase letters
+
+Password will include symbols
+
+In the 2.0 Update now the order is randomized so passwords are even harder to crack because of the no pattern to track
+
+**Note if you try to use the confirm password field as password, the two results will be different.
+
 */
 
 function shuffle() {
     //Getting array of alphabet
-    var myArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    var myArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '@', '!', '#', '$', '%', '^', '&', '*', '(', ')', '+', '`', '~', '@', '!', '#', '$', '%', '^', '&', '*', '(', ')', '+', '`', '~', '1', '2', '3', '4', '5', '6', '7', '8', '9', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
     //Selecting one letter from array randomly
     var rand = myArray[Math.floor(Math.random() * myArray.length)];
@@ -18,16 +27,13 @@ function shuffle() {
 }
 
 function key() {
-    //Chosing the numbers one by one
-    var firstNumber = Math.floor((Math.random() * 9) + 1);
-    var secondNumber = Math.floor((Math.random() * 9) + 1);
-
-    //Chosing the letters one by one
-    var firstLetter = shuffle();
-    var secondLetter = shuffle();
+    var firstChar = shuffle();
+    var secondChar = shuffle();
+    var thirdChar = shuffle();
+    var fourthChar = shuffle();
 
     //Concatinating indiviual values together as a set of 4
-    var pin = firstNumber+firstLetter+secondNumber+secondLetter;
+    var pin = firstChar+secondChar+thirdChar+fourthChar;
     
     //Returning the set of 4
     return pin;
@@ -35,13 +41,13 @@ function key() {
 
 function generate() {
     //Declaring sets
-    var text1 = key();
-    var text2 = key();
-    var text3 = key();
-    var text4 = key();
+    var set1 = key();
+    var set2 = key();
+    var set3 = key();
+    var set4 = key();
 
-    //Concatinating sets together to create a 16 combination(19 Charecters toral with dashes)
-    var final = text1 + "-" + text2 + "-" + text3 + "-" + text4;
+    //Concatinating sets together to create a 16 combination(19 Charecters total with dashes)
+    var final = set1 + "-" + set2 + "-" + set3 + "-" + set4;
 
     //Returning long password
     return final;

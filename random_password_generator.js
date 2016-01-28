56467a18da7854435937fdf7fd0360a56ab52bcf
cesar <cesarguerrero2199@yahoo.com>
2016-01-28 16:27:11 -0500

    Made updates to no longer use the jQuery lib

--- random_password_generator.js
+++ random_password_generator.js
@@ -1,29 +1,19 @@
 //Random password generator v2.0
-/*
-
-Revisions:
-
-Can now use them as classes instead of id's, which means you can have the confirm password field show up many times
-    
-Passwords will include uppercase letters
-
-Password will include symbols
-
-In the 2.0 Update now the order is randomized so passwords are even harder to crack because of the no pattern to track
-
-**Note if you try to use the confirm password field as password, the two results will be different.
-
-*/
 
 function shuffle() {
     //Getting array of alphabet
-    var myArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '@', '!', '#', '$', '%', '^', '&', '*', '(', ')', '+', '`', '~', '@', '!', '#', '$', '%', '^', '&', '*', '(', ')', '+', '`', '~', '1', '2', '3', '4', '5', '6', '7', '8', '9', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+    var myArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+        'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+        's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C',
+        'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O',
+        'P', 'Q', 'R', 'S', 'T', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '@',
+        '!', '#', '$', '%', '^', '&', '*', '(', ')', '+', '`', '~', '@',
+        '!', '#', '$', '%', '^', '&', '*', '(', ')', '+', '`', '~', '1',
+        '2', '3', '4', '5', '6', '7', '8', '9', '1', '2', '3', '4', '5',
+        '6', '7', '8', '9'];
 
     //Selecting one letter from array randomly
-    var rand = myArray[Math.floor(Math.random() * myArray.length)];
-
-    //Returning the randomly selected letter
-    return rand;
+    return myArray[Math.floor(Math.random() * myArray.length)];
 }
 
 function key() {
@@ -33,10 +23,7 @@ function key() {
     var fourthChar = shuffle();
 
     //Concatinating indiviual values together as a set of 4
-    var pin = firstChar+secondChar+thirdChar+fourthChar;
-    
-    //Returning the set of 4
-    return pin;
+    return firstChar+secondChar+thirdChar+fourthChar;
 }
 
 function generate() {
@@ -47,20 +34,14 @@ function generate() {
     var set4 = key();
 
     //Concatinating sets together to create a 16 combination(19 Charecters total with dashes)
-    var final = set1 + "-" + set2 + "-" + set3 + "-" + set4;
-
-    //Returning long password
-    return final;
+    return set1 + "-" + set2 + "-" + set3 + "-" + set4;
 }
 
+function randomPassword()
+{
+    var generated = generate();
 
-$("#button").click(function () {
-    //Setting the value of the password field with what was generated
-    var $pass = $(".password").val(generate);
-
-    //Setting was on the password field to the confirm password field
-    $(".c_password").val($pass.val());
-
-    //How to show user what was generated
-    $("#getter").text($pass.val());
-});
+    document.getElementById("password").value = generated;
+    document.getElementById("confirm").value = generated;
+    document.getElementById("getter").innerHTML = generated;
+}
